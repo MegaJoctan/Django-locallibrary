a5d799616b02808059760f16e55db4ded3f1f7bf
Omega Joctan <omegajoctan@gmail.com>
2021-12-06 13:44:03 +0300

    heroku db setup

--- LocalLibrary/LocalLibrary/settings.py
+++ LocalLibrary/LocalLibrary/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'LocalLibrary.wsgi.application'
 DATABASES = {}
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'].db_from_env
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
